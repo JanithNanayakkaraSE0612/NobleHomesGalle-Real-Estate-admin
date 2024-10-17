d75c238164ac39217188fea6623a0e95ade4b515
Janith-Nanayakkara <141998810+JanithNanayakkaraSE0612@users.noreply.github.com>
2024-10-17 14:07:37 +0530

    Update

--- admin/src/Pages/dashboard/index.js
+++
@@ -1,7 +0,0 @@
-export * from "@/pages/dashboard/home";
-export * from "@/pages/dashboard/profile";
-export * from "@/pages/dashboard/tables";
-export * from "@/pages/dashboard/notifications";
-export * from "@/pages/dashboard/agent";
-export * from "@/pages/dashboard/agentDetails";
-export * from "@/pages/dashboard/newAgent";
